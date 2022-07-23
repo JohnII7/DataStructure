508c87679abd470d5b7a903dcb295e53c6b28698
JohnII7 <157798378@qq.com>
2022-07-23 11:52:15 +0800

    二分法

--- main.c
+++ main.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
-int search(int *nums,int numsSize,int target){
-    for (int i = 0; i < numsSize; ++i) {
-        if(nums[i]==target) return i;
-    }
-    return -1;
+
+int binarySearch(int *nums, int target, int left, int right) {
+    int mid = (left+right)/2;
+    if(nums[mid] == target) return mid;
+    if (nums[mid]<target) return binarySearch(nums,target,mid+1,right);
+    else return binarySearch(nums , target,left,mid -1);
+}
+
+int search(int *nums, int numsSize, int target) {
+    return binarySearch(nums,target,0,numsSize-1);
 }
 
 int main() {
-    int arr[] = {1,3,4,6,7,8,10,11,13,15},target = 3;
-    printf("%d",search(arr,10,target));
+    int arr[] = {1, 3, 4, 6, 7, 8, 10, 11, 13, 15}, target = 3;
+    printf("%d", search(arr, 10, target));
 }
