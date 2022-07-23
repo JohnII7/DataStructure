e4a3dba05c9627958b49879c7a7241cb67351a92
JohnII7 <157798378@qq.com>
2022-07-23 10:52:35 +0800

    二分法暴力解法

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+int search(int *nums,int numsSize,int target){
+    for (int i = 0; i < numsSize; ++i) {
+        if(nums[i]==target) return i;
+    }
+    return -1;
+}
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    int arr[] = {1,3,4,6,7,8,10,11,13,15},target = 3;
+    printf("%d",search(arr,10,target));
 }
