377aac8576c690f632609070252303f38d17d6d0
JohnII7 <157798378@qq.com>
2022-07-23 17:19:41 +0800

    二分法优化

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int binarySearch(int *nums, int target, int left, int right) {
-    int mid = (left+right)/2;
-    if(nums[mid] == target) return mid;
-    if (nums[mid]<target) return binarySearch(nums,target,mid+1,right);
-    else return binarySearch(nums , target,left,mid -1);
-}
 
 int search(int *nums, int numsSize, int target) {
-    return binarySearch(nums,target,0,numsSize-1);
+    int left = 0, right = numsSize - 1;
+    while (left <= right) {
+        int mid = (right + left) / 2;
+        if (nums[mid] == target) return mid;
+        if (nums[mid] < target) left = mid + 1;
+        if (nums[mid] > target) right = mid - 1;
+    }
+    return -1;
 }
 
 int main() {
-    int arr[] = {1, 3, 4, 6, 7, 8, 10, 11, 13, 15}, target = 3;
+    int arr[] = {1, 3, 4, 6, 7, 8, 10, 11, 13, 15}, target = 13;
     printf("%d", search(arr, 10, target));
 }
